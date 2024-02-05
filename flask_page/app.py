76ff71247fb09e06dbcc8e2b3786e5d7fb02b865
KMG <rlaaudrb1104@gmail.com>
2024-02-05 17:50:06 +0900

    XSS 플라스크 연동

--- flask_page/app.py
+++ flask_page/app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 from input_tag_analysis import input_tag_bp
 from comments_analysis import comments_bp
+from XSS_analysis import XSS_Bypass_bp
+
 
 app = Flask(__name__)
 
 # 블루프린트 등록
 app.register_blueprint(input_tag_bp)
 app.register_blueprint(comments_bp)
+app.register_blueprint(XSS_Bypass_bp)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/xss')
-def xss_page():
-    return render_template('xss.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
