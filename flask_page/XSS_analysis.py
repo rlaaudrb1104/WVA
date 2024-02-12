421dfbf9d95d81e79bd41a8b6c15118121debe08
KMG <rlaaudrb1104@gmail.com>
2024-02-12 23:40:43 +0900

    XSS_Payload 연동

--- flask_page/XSS_analysis.py
+++ flask_page/XSS_analysis.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, request
-from XSS_parser import XSS_Bypass
+from flask import Blueprint, request, jsonify, render_template
 import html
-XSS_Bypass_bp = Blueprint('XSS_Bypass_bp', __name__, template_folder='templates')
+from XSS_parser import XSS_Bypass
+from XSS_parser import XSS_Payload
 
+XSS_Bypass_bp = Blueprint('XSS_Bypass_bp', __name__, template_folder='templates')
+XSS_Payload_bp = Blueprint('XSS_Payload_bp', __name__, template_folder='templates')
 @XSS_Bypass_bp.route('/XSS', methods=['GET','POST'])
 def XSS():
   if request.method == 'POST':
@@ -25,3 +27,17 @@ def XSS():
     Bypass_result = html.escape(result)
     return render_template('XSS_Bypass_result.html', Bypass_result=Bypass_result)
   return render_template('XSS.html')
+
+@XSS_Payload_bp.route('/XSS', methods=['GET', 'POST'])
+def XSS_P():
+    if request.method == 'POST':
+        user_provided_url = request.form['url']
+        user_provided_params = request.form['params']
+        
+        result = XSS_Payload(user_provided_url, user_provided_params)
+        # 결과 문자열을 HTML 엔티티로 이스케이프 처리
+        Payload_result = html.escape(result)
+        # 결과를 JSON 형태로 클라이언트에 반환
+        return jsonify({'Payload_result': Payload_result})
+    # GET 요청 시, XSS 테스트 폼을 포함한 HTML 페이지 렌더링
+    return render_template('XSS.html')
--- flask_page/app.py
+++ flask_page/app.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template
+
 from input_tag_analysis import input_tag_bp
 from XSS_analysis import XSS_Bypass_bp
-
+from XSS_analysis import XSS_Payload_bp
+from flask_socketio import SocketIO,emit
+import threading
+from XSS_parser import XSS_Payload
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 
 # 블루프린트 등록
 app.register_blueprint(input_tag_bp)
 app.register_blueprint(XSS_Bypass_bp)
-
+app.register_blueprint(XSS_Payload_bp)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/XSS_Payload', methods=['GET'])
+def xss_payload_form():
+    return render_template('XSS_Payload.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@socketio.on('test_xss')
+def handle_test_xss(json):
+    # 별도의 스레드에서 XSS 테스트 실행
+    thread = threading.Thread(target=XSS_Payload, args=(json['url'], json['param'], socketio))
+    thread.start()
+    
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
