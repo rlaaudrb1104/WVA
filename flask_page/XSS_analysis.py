bcd4e40388c45e817a3a811dd8d4b6eeb639c982
KMG <rlaaudrb1104@gmail.com>
2024-02-14 15:09:25 +0900

    XSS 프론트 연결

--- flask_page/XSS_analysis.py
+++ flask_page/XSS_analysis.py
@@ -7,26 +7,26 @@ XSS_Bypass_bp = Blueprint('XSS_Bypass_bp', __name__, template_folder='templates'
 XSS_Payload_bp = Blueprint('XSS_Payload_bp', __name__, template_folder='templates')
 @XSS_Bypass_bp.route('/XSS', methods=['GET','POST'])
 def XSS():
-  if request.method == 'POST':
-    user_provided_url = request.form['url']
-    
-    user_provided_cookies = [
-            {
-                'name': request.form.get('cookie_name1', ''),
-                'value': request.form.get('cookie_value1', '')
-            },
-            {
-                'name': request.form.get('cookie_name2', ''),
-                'value': request.form.get('cookie_value2', '')
-            }
-        ]
+    if request.method == 'POST':
+        user_provided_url = request.form['url']
         
-    user_provided_params = request.form['params']
-    
-    result = XSS_Bypass(user_provided_url,user_provided_params)
-    Bypass_result = html.escape(result)
-    return render_template('XSS_Bypass_result.html', Bypass_result=Bypass_result)
-  return render_template('XSS.html')
+        user_provided_cookies = [
+                {
+                    'name': request.form.get('cookie_name1', ''),
+                    'value': request.form.get('cookie_value1', '')
+                },
+                {
+                    'name': request.form.get('cookie_name2', ''),
+                    'value': request.form.get('cookie_value2', '')
+                }
+            ]
+            
+        user_provided_params = request.form['params']
+        
+        result = XSS_Bypass(user_provided_url,user_provided_params)
+        Bypass_result = html.escape(result)
+        return render_template('XSS_Bypass_result.html', Bypass_result=Bypass_result)
+    return render_template('XSS.html')
 
 @XSS_Payload_bp.route('/XSS', methods=['GET', 'POST'])
 def XSS_P():
